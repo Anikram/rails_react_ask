582a0b80814726424af223ee0759eb8a08412d85
Anikram <4nikram@gmail.com>
2021-11-16 21:55:28 +0300

    react function components, hooks and style test

--- app/javascript/components/Hello/HelloWorld.js
+++ app/javascript/components/Hello/HelloWorld.js
@@ -1,13 +1,21 @@
-import React from "react"
+import React, {useState} from "react"
 import PropTypes from "prop-types"
 import styles from './index.module.scss'
 
 function HelloWorld(props) {
+  const [isActive, setIsActive] = useState()
+  const clickHandler = () => {
+    setIsActive(prev => !prev)
+  }
+
   return (
     <div className={styles.tab}>
       Hello!
       {props.greeting}
       {props.anotherGreating}
+      <div className={`${styles.button} ${isActive ? styles.active : ''}`} onClick={clickHandler}>
+        Push me
+      </div>
     </div>
   );
 }
