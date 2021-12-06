3c9626db2e0739e37d0e2a8acaa1a4a024d144cb
Anikram <4nikram@gmail.com>
2021-12-06 16:12:22 +0300

    hedaer styles changed. user_avatar partial added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,19 @@
 class UsersController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
   def show
-    @hi = 'Hi!'
+    @user = User.new(
+      first_name: 'Sam',
+      last_name: 'Wise',
+      avatar_url: ''
+    )
+    # https://ru.gravatar.com/userimage/138026325/db741d48f05a99f388bc632b7bc3f70e.jpg?size=200
   end
 end
